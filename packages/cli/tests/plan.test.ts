18a6511b624dd17bd9fd37524e8be741b7d7d833
IzumiSy <beetle-noise@gmx.com>
2025-08-18 17:07:12 +0900

    Updated plan test data

--- packages/cli/tests/plan.test.ts
+++ packages/cli/tests/plan.test.ts
@@ -11,24 +11,6 @@ vi.mock("fs/promises", async () => {
 });
 
 const { database, client } = await setupTestDB();
-const config = defineConfigForTest({
-  database,
-  tables: [
-    defineTable(
-      "members",
-      {
-        id: column("uuid", { primaryKey: true }),
-        name: column("text", { unique: true }),
-        email: column("text"),
-      },
-      (t) => [t.index(["name", "email"])]
-    ),
-    defineTable("category", {
-      id: column("uuid", { primaryKey: true }),
-      name: column("text", { unique: true }),
-    }),
-  ],
-});
 
 it("generate with planned apply", async () => {
   const loggerStdout = vi
@@ -38,7 +20,29 @@ it("generate with planned apply", async () => {
   await runGenerate({
     client,
     logger: defaultConsolaLogger,
-    config,
+    config: defineConfigForTest({
+      database,
+      tables: [
+        defineTable(
+          "members",
+          {
+            id: column("uuid", { primaryKey: true }),
+            name: column("text", { unique: true }),
+            email: column("text"),
+          },
+          (t) => [t.index(["name", "email"]), t.unique(["name", "email"])]
+        ),
+        defineTable(
+          "category",
+          {
+            id: column("uuid"),
+            member_id: column("uuid"),
+            name: column("text", { unique: true }),
+          },
+          (t) => [t.primaryKey(["id", "member_id"])]
+        ),
+      ],
+    }),
     options: {
       ignorePending: false,
       apply: true,
@@ -85,18 +89,20 @@ it("generate with planned apply", async () => {
 
   [
     // 1st time
-    `create table "category" ("id" uuid not null, "name" text)`,
+    `create table "category" ("id" uuid, "member_id" uuid, "name" text)`,
     `create table "members" ("id" uuid not null, "name" text, "email" text)`,
     `create index "idx_members_name_email" on "members" ("name", "email")`,
-    `alter table "category" add constraint "category_id_primary_key" primary key ("id")`,
+    `alter table "category" add constraint "pk_category_id_member_id" primary key ("id", "member_id")`,
     `alter table "members" add constraint "members_id_primary_key" primary key ("id")`,
     `alter table "category" add constraint "category_name_unique" unique ("name")`,
+    `alter table "members" add constraint "uq_members_name_email" unique ("name", "email")`,
     `alter table "members" add constraint "members_name_unique" unique ("name")`,
 
     // 2nd time
     `alter table "category" drop constraint "category_name_unique"`,
     `alter table "members" drop constraint "members_name_unique"`,
-    `alter table "category" drop constraint "category_id_primary_key"`,
+    `alter table "members" drop constraint "uq_members_name_email"`,
+    `alter table "category" drop constraint "pk_category_id_member_id"`,
     `drop index "idx_members_name_email"`,
     `drop table "category"`,
     `create table "posts" ("id" uuid not null, "content" text)`,
