1e61e62847ced1e58e3382cbafcb6ff00e7b59a3
IzumiSy <beetle-noise@gmx.com>
2025-08-19 15:47:25 +0900

    Use foreign key in plan test

--- packages/cli/tests/plan.test.ts
+++ packages/cli/tests/plan.test.ts
@@ -17,78 +17,105 @@ it("generate with planned apply", async () => {
     .spyOn(defaultConsolaLogger, "stdout")
     .mockImplementation(() => void 0);
 
-  await runGenerate({
-    client,
-    logger: defaultConsolaLogger,
-    config: defineConfigForTest({
-      database,
-      tables: [
-        defineTable(
-          "members",
-          {
-            id: column("uuid", { primaryKey: true }),
-            name: column("text", { unique: true }),
-            email: column("text"),
-          },
-          (t) => [t.index(["name", "email"]), t.unique(["name", "email"])]
-        ),
-        defineTable(
-          "category",
-          {
-            id: column("uuid"),
-            member_id: column("uuid"),
-            name: column("text", { unique: true }),
-          },
-          (t) => [t.primaryKey(["id", "member_id"])]
-        ),
-      ],
-    }),
-    options: {
-      ignorePending: false,
-      apply: true,
-      plan: true,
-    },
-  });
+  // 1st phase
+  {
+    const membersTable = defineTable(
+      "members",
+      {
+        id: column("uuid", { primaryKey: true }),
+        name: column("text", { unique: true }),
+        email: column("text"),
+      },
+      (t) => [t.index(["name", "email"]), t.unique(["name", "email"])]
+    );
 
-  await runApply({
-    client,
-    logger: defaultConsolaLogger,
-    options: {
-      plan: false,
-      pretty: false,
-    },
-  });
+    await runGenerate({
+      client,
+      logger: defaultConsolaLogger,
+      config: defineConfigForTest({
+        database,
+        tables: [
+          membersTable,
+          defineTable(
+            "category",
+            {
+              id: column("uuid"),
+              member_id: column("uuid"),
+              name: column("text", { unique: true }),
+            },
+            (t) => [
+              t.primaryKey(["id", "member_id"]),
+              t.reference("member_id", membersTable, "id", {
+                onDelete: "cascade",
+                name: "category_member_fk",
+              }),
+            ]
+          ),
+        ],
+      }),
+      options: {
+        ignorePending: false,
+        apply: true,
+        plan: true,
+      },
+    });
 
-  await runGenerate({
-    client,
-    logger: defaultConsolaLogger,
-    config: defineConfigForTest({
-      database,
-      tables: [
-        defineTable(
-          "members",
-          {
-            id: column("uuid", { primaryKey: true }),
-            name: column("text"),
-            email: column("text", { unique: true }),
-          },
-          (t) => [t.index(["id", "email"], { unique: true })]
-        ),
-        defineTable("posts", {
-          id: column("uuid", { primaryKey: true }),
-          content: column("text"),
-        }),
-      ],
-    }),
-    options: {
-      ignorePending: false,
-      apply: true,
-      plan: true,
-    },
-  });
+    await runApply({
+      client,
+      logger: defaultConsolaLogger,
+      options: {
+        plan: false,
+        pretty: false,
+      },
+    });
+  }
+
+  // 2nd phase
+  {
+    const membersTable = defineTable(
+      "members",
+      {
+        id: column("uuid", { primaryKey: true }),
+        name: column("text"),
+        email: column("text", { unique: true }),
+      },
+      (t) => [t.index(["id", "email"], { unique: true })]
+    );
+
+    await runGenerate({
+      client,
+      logger: defaultConsolaLogger,
+      config: defineConfigForTest({
+        database,
+        tables: [
+          membersTable,
+          defineTable(
+            "posts",
+            {
+              id: column("uuid", { primaryKey: true }),
+              content: column("text"),
+              author_id: column("uuid", { notNull: true }),
+            },
+            (t) => [
+              t.reference("author_id", membersTable, "id", {
+                onDelete: "set null",
+                onUpdate: "cascade",
+                name: "posts_author_fk",
+              }),
+            ]
+          ),
+        ],
+      }),
+      options: {
+        ignorePending: false,
+        apply: true,
+        plan: true,
+      },
+    });
+  }
 
   [
-    // 1st time
+    // 1st phase
     `create table "category" ("id" uuid, "member_id" uuid, "name" text)`,
     `create table "members" ("id" uuid not null, "name" text, "email" text)`,
     `create index "idx_members_name_email" on "members" ("name", "email")`,
@@ -97,18 +124,21 @@ it("generate with planned apply", async () => {
     `alter table "category" add constraint "category_name_unique" unique ("name")`,
     `alter table "members" add constraint "uq_members_name_email" unique ("name", "email")`,
     `alter table "members" add constraint "members_name_unique" unique ("name")`,
+    `alter table "category" add constraint "category_member_fk" foreign key ("member_id") references "members" ("id") on delete cascade`,
 
-    // 2nd time
+    // 2nd phase
+    `alter table "category" drop constraint "category_member_fk"`,
     `alter table "category" drop constraint "category_name_unique"`,
     `alter table "members" drop constraint "members_name_unique"`,
     `alter table "members" drop constraint "uq_members_name_email"`,
     `alter table "category" drop constraint "pk_category_id_member_id"`,
     `drop index "idx_members_name_email"`,
     `drop table "category"`,
-    `create table "posts" ("id" uuid not null, "content" text)`,
+    `create table "posts" ("id" uuid not null, "content" text, "author_id" uuid not null)`,
     `create unique index "idx_members_id_email" on "members" ("id", "email")`,
     `alter table "posts" add constraint "posts_id_primary_key" primary key ("id")`,
     `alter table "members" add constraint "members_email_unique" unique ("email")`,
+    `alter table "posts" add constraint "posts_author_fk" foreign key ("author_id") references "members" ("id") on delete set null on update cascade`,
   ].forEach((expectedCall, index) => {
     expect(loggerStdout).toHaveBeenNthCalledWith(index + 1, expectedCall);
   });
