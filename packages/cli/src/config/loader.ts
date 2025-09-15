12856c622982ccf15a51a51433ffc32084f541a2
IzumiSy <beetle-noise@gmx.com>
2025-09-15 16:46:22 +0900

    Remove SQLite dialect

--- packages/cli/src/config/loader.ts
+++ packages/cli/src/config/loader.ts
@@ -30,7 +30,7 @@ const indexSchema = z.object({
 });
 export type IndexSchema = z.infer<typeof indexSchema>;
 
-const dialectEnum = z.enum(["postgres", "cockroachdb", "sqlite"]);
+const dialectEnum = z.enum(["postgres", "cockroachdb"]);
 export type DialectEnum = z.infer<typeof dialectEnum>;
 
 const databaseSchema = z.object({
--- packages/cli/src/dialect/factory.ts
+++ packages/cli/src/dialect/factory.ts
@@ -1,13 +1,11 @@
 import { KyrageDialect } from "./types";
 import { PostgresKyrageDialect } from "./postgres";
 import { CockroachDBKyrageDialect } from "./cockroachdb";
-import { SQLiteKyrageDialect } from "./sqlite";
 import { DialectEnum } from "../config/loader";
 
 const dialects = {
   postgres: new PostgresKyrageDialect(),
   cockroachdb: new CockroachDBKyrageDialect(),
-  sqlite: new SQLiteKyrageDialect(),
 } as const;
 
 export const getDialect = (dialectName: DialectEnum) => {
